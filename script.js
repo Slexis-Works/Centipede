1b3c15078d52e5fac00f1ded3dcaac90f80004bc
Slexis-Works <cabodi.alexis@orange.fr>
2016-03-20 20:57:38 +0100

    Intégration et ajustement du squelette JS

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
+// Variables (globales) du programme
 
+var cnv = null, ctx = null;
+
+init = function() {
+    // instanciation de la variable globale contenant le contexte
+    cnv = document.getElementById("cnv");
+    ctx = cnv.getContext("2d");
+
+    // 2 écouteurs pour le clavier (appui/relâchement d'une touche)
+    document.addEventListener("keydown", captureAppuiToucheClavier)
+    document.addEventListener("keyup", captureRelacheToucheClavier)
+    // on associe au document un écouteur d'événements souris
+    //document.addEventListener("click", captureClicSouris)
+
+    // lancement de la boucle de jeu
+    boucleDeJeu();
+}
+
+
+boucleDeJeu = function() {
+    // mise à jour de l'état du jeu
+    update(Date.now());
+    // affichage de l'état du jeu
+    render();
+    // rappel de la boucle de jeu
+    requestAnimationFrame(boucleDeJeu);
+}
+
+
+/**
+ *  Mise à jour de l'état du jeu
+ *  @param  d   date courante
+ */
+update = function(d) {
+
+}
+
+
+/**
+ *  Fonction réalisant le rendu de l'état du jeu
+ */
+render = function() {
+    // effacement de l'écran
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+}
+
+
+/**
+ *  Fonction appelée lorsqu'une touche du clavier est appuyée
+ *  Associée à l'événement "keyDown"
+ */
+captureAppuiToucheClavier = function(event) {
+    // pratique pour connaître les keyCode des touches du clavier :
+    //  --> http://www.cambiaresearch.com/articles/15/javascript-key-codes
+}
+
+/**
+ *  Fonction appelée lorsqu'une touche du clavier est relâchée
+ *  Associée à l'événement "keyUp"
+ */
+captureRelacheToucheClavier = function(event) {
+
+}
+
+/**
+ *  Fonction appelée lorsqu'une touche du clavier est relâchée
+ *  Associée à l'événement "click"
+ */
+// Inutile pour la version basique du jeu
+/* captureClicSouris = function(event) {
+    // calcul des coordonnées de la souris dans le canvas
+    if (event.target.id == "cvs") {
+        clic.x = event.pageX - event.target.offsetLeft;
+        clic.y = event.pageY - event.target.offsetTop;
+    }
+} */
