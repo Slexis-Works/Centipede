e02c324017f70a6a31723aad5e6e88f93bdfebd0
Slexis-Works <cabodi.alexis@orange.fr>
2016-03-22 16:44:02 +0100

    Ajout des variables statiques pour les touches

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 
 var cnv = null, ctx = null;
 
+// Touches clavier
+var toucheHaut = false, toucheBas = false,
+    toucheGauche = false, toucheDroite = false;
+var toucheZ = false, toucheS = false,
+    toucheQ = false, toucheD = false;
+var appuiHaut = false, appuiBas = false,
+    appuiGauche = false, appuiDroite = false;
+var appuiTir = false;
+
 init = function() {
     // instanciation de la variable globale contenant le contexte
     cnv = document.getElementById("cnv");
@@ -53,6 +62,36 @@ render = function() {
 captureAppuiToucheClavier = function(event) {
     // pratique pour connaître les keyCode des touches du clavier :
     //  --> http://www.cambiaresearch.com/articles/15/javascript-key-codes
+    switch (event.keyCode) {
+      case 38:
+        toucheHaut = true;
+        break;
+      case 40:
+        toucheBas = true;
+        break;
+      case 37:
+        toucheGauche = true;
+        break;
+      case 39:
+        toucheDroite = true;
+        break;
+      case 90:
+        toucheZ = true;
+        break;
+      case 83:
+        toucheS = true;
+        break;
+      case 81:
+        toucheQ = true;
+        break;
+      case 68:
+        toucheD = true;
+        break;
+      case 32:
+        appuiTir = true;
+    }
+
+    miseAJourAppuis();
 }
 
 /**
@@ -60,7 +99,47 @@ captureAppuiToucheClavier = function(event) {
  *  Associée à l'événement "keyUp"
  */
 captureRelacheToucheClavier = function(event) {
+    switch (event.keyCode) {
+      case 38:
+        toucheHaut = false;
+        break;
+      case 40:
+        toucheBas = false;
+        break;
+      case 37:
+        toucheGauche = false;
+        break;
+      case 39:
+        toucheDroite = false;
+        break;
+      case 90:
+        toucheZ = false;
+        break;
+      case 83:
+        toucheS = false;
+        break;
+      case 81:
+        toucheQ = false;
+        break;
+      case 68:
+        toucheD = false;
+        break;
+      case 32:
+        appuiTir = false;
+    }
+  miseAJourAppuis();
+}
 
+miseAJourAppuis = function() {
+  appuiHaut = toucheHaut || toucheZ;
+  appuiBas = toucheBas || toucheS;
+  appuiGauche = toucheGauche || toucheQ;
+  appuiDroite = toucheDroite || toucheD;
+  //console.log((appuiHaut)?"Touche Haut appuyée":"Touche Haut relâchée.");
+  //console.log((appuiBas)?"Touche Bas appuyée":"Touche Bas relâchée.");
+  //console.log((appuiGauche)?"Touche Gauche appuyée":"Touche Gauche relâchée.");
+  //console.log((appuiDroite)?"Touche Droite appuyée":"Touche Droite relâchée.");
+  //console.log((appuiTir)?"Touche Tir appuyée":"Touche Tir relâchée.");
 }
 
 /**
